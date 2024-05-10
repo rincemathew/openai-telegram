e30837b628458aedceb44bea92e6c0b4f659a8a4
Rince Mathew <rincemathewmanattu@gmail.com>
2024-05-10 14:13:27 +0530

    first version completed

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import OpenAI from "openai";
 
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-
-const openai = new OpenAI.OpenAI({project: 'proj_JWVPjsDeGT3qO7WPSXtipv7U',
-apiKey:process.env.OPENAI_API_KEY})
-
+const openai = new OpenAI.OpenAI({
+  project: "proj_JWVPjsDeGT3qO7WPSXtipv7U",
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 async function main() {
+  try {
     const chatCompletion = await openai.chat.completions.create({
-        model:'gpt-3.5-turbo',
-        messages: [
-            { role:'user', content:'wHAT IS THE CAPITAL OF INDIA?'},
-        ],
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "wHAT IS THE CAPITAL OF INDIA?" }],
     });
-    console.log(chatCompletion.data.choices[0].message.content)
+    console.log(chatCompletion.choices[0].message.content);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
