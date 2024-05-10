11b90f2f45d37d6c7993731ea0feff41306ec3f6
Rince Mathew <rincemathewmanattu@gmail.com>
2024-05-10 13:28:29 +0530

    connected to chat gpt

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
 import dotenv from 'dotenv';
 dotenv.config()
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAI.OpenAI({project: 'proj_JWVPjsDeGT3qO7WPSXtipv7U',
+apiKey:process.env.OPENAI_API_KEY})
+
+
+async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+        model:'gpt-3.5-turbo',
+        messages: [
+            { role:'user', content:'wHAT IS THE CAPITAL OF INDIA?'},
+        ],
+    });
+    console.log(chatCompletion.data.choices[0].message.content)
+}
+
+main();
